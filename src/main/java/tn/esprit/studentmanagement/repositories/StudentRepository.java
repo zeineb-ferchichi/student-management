c32f9c35d5baa21d73049435cfe98c9ae818d179
zeineb-ferchichi <ferchichizeineb56@gmail.com>
2026-03-01 14:05:11 +0100

    Clean up imports in StudentRepository
    Removed unused import statements for Course.

--- src/main/java/tn/esprit/studentmanagement/repositories/StudentRepository.java
+++ src/main/java/tn/esprit/studentmanagement/repositories/StudentRepository.java
@@ -1,8 +1,7 @@
 package tn.esprit.studentmanagement.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.studentmanagement.entities.Course;
+import org.springframework.stereotype.Repository
 import tn.esprit.studentmanagement.entities.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
