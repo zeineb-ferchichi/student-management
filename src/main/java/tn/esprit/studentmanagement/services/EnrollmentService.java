cb13f541b19fa33867332049e06f9b62641e1cc6
zeineb-ferchichi <ferchichizeineb56@gmail.com>
2026-03-01 14:07:51 +0100

    Remove unused import for Lombok AllArgsConstructor
    Removed unused Lombok AllArgsConstructor import.

--- src/main/java/tn/esprit/studentmanagement/services/EnrollmentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/EnrollmentService.java
@@ -1,6 +1,5 @@
 package tn.esprit.studentmanagement.services;
 
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tn.esprit.studentmanagement.repositories.EnrollmentRepository;
